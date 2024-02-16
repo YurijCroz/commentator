55c89513ad4263a84e04f0338771d487cb7290a7
YurijCroz <valetkrest@gmail.com>
2024-02-16 09:46:00 +0200

    hot fix login user unavailability

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -8,6 +8,7 @@ const updateUser = require("../queries/updateUser");
 const NotUniqueEmail = require("../errors/NotUniqueEmail");
 const TokenError = require("../errors/TokenError");
 const ServerError = require("../errors/ServerError");
+const UserNotFoundError = require("../errors/UserNotFoundError");
 
 const {
   JWT_SECRET_ACCESS,
@@ -20,21 +21,26 @@ const {
 module.exports.registrationUser = async (req, res, next) => {
   try {
     const foundUser = await findUser({ email: req.body.email });
+
     if (foundUser) {
-      next(new NotUniqueEmail());
+      throw new NotUniqueEmail();
     }
+
     const body = {
       userName: req.body.userName,
       email: req.body.email,
       homePage: req.body.homePage,
       password: bcrypt.hashSync(req.body.password, +SALT_ROUNDS),
     };
+
     const newUser = await User.create({
       ...body,
     });
+
     if (!newUser) {
       next(new ServerError());
     }
+
     res.status(200).send({ massage: "success" });
   } catch (error) {
     next(error);
@@ -44,7 +50,13 @@ module.exports.registrationUser = async (req, res, next) => {
 module.exports.login = async (req, res, next) => {
   try {
     const foundUser = await findUser({ email: req.body.email });
+
+    if (!foundUser) {
+      throw new UserNotFoundError("Invalid email or password");
+    }
+
     await passwordCompare(req.body.password, foundUser.password);
+
     const accessToken = getJwtToken(
       foundUser,
       JWT_SECRET_ACCESS,
@@ -55,7 +67,9 @@ module.exports.login = async (req, res, next) => {
       JWT_SECRET_REFRESH,
       REFRESH_TOKEN_TIME
     );
+
     await updateUser({ refreshToken }, foundUser.userId);
+
     res.send({
       tokensPair: { accessToken, refreshToken },
     });
@@ -67,6 +81,7 @@ module.exports.login = async (req, res, next) => {
 module.exports.refreshToken = async (req, res, next) => {
   try {
     const foundUser = await findUser({ userId: req.refreshTokenData.userId });
+
     if (foundUser.refreshToken === req.body.refreshToken) {
       const accessToken = getJwtToken(
         foundUser,
@@ -78,7 +93,9 @@ module.exports.refreshToken = async (req, res, next) => {
         JWT_SECRET_REFRESH,
         REFRESH_TOKEN_TIME
       );
+
       await updateUser({ refreshToken }, foundUser.userId);
+
       res.send({
         tokensPair: { accessToken, refreshToken },
       });
--- utils/passwordCompare.js
+++ utils/passwordCompare.js
@@ -1,11 +1,15 @@
 "use strict";
-const NotFound = require("../errors/UserNotFoundError");
+const UserNotFoundError = require("../errors/UserNotFoundError");
 const bcrypt = require("bcrypt");
 
 const passwordCompare = async (pass1, pass2) => {
-  const passwordCompare = await bcrypt.compare(pass1, pass2);
-  if (!passwordCompare) {
-    throw new NotFound("Wrong password");
+  try {
+    const passwordCompare = await bcrypt.compare(pass1, pass2);
+    if (!passwordCompare) {
+      throw new UserNotFoundError("Invalid email or password");
+    }
+  } catch (error) {
+    throw error;
   }
 };
 
